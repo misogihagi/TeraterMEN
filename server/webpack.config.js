2561276c60649514d70c0fc24ffc3f981b97c62a
misogihagi <hagimiso@gmail.com>
2021-01-17 17:40:28 +0900

    delete unused plugin nodeexternal

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   mode: process.env === 'prodction' ? 'prodction' : 'development' ,
